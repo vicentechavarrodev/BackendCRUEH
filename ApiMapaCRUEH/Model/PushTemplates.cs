4f33b7bcac8e149a835ff2bdd25e114a84df1189
Vicente Chavarro <reyesvrc@gmail.com>
2026-04-22 22:55:38 -0500

    se modifican plantilla de Android

--- ApiMapaCRUEH/Model/PushTemplates.cs
+++ ApiMapaCRUEH/Model/PushTemplates.cs
@@ -4,17 +4,14 @@
 		{
 				public class Generic
 				{
-						public const string Android = "{\"message\":{\"android\":{\"priority\":\"high\",\"notification\":{\"title\":\"$(alertTitle)\",\"body\":\"$(alertMessage)\",\"tag\":\"app_unica\"}},\"data\":{\"action\":\"$(alertAction)\",\"idEvento\":\"$(idEvento)\",\"detalles\":\"$(extraData)\"}}}";
-						//public const string Android = "{\"message\":{\"notification\":{\"title\":\"$(alertTitle)\",\"body\":\"$(alertMessage)\"},\"android\":{\"priority\":\"high\"},\"data\":{\"action\":\"$(alertAction)\",\"idEvento\":\"$(idEvento)\",\"detalles\":\"$(extraData)\"}}}";
+						public const string Android = "{\"message\":{\"android\":{\"priority\":\"high\"},\"data\":{\"title\":\"$(alertTitle)\",\"body\":\"$(alertMessage)\",\"action\":\"$(alertAction)\",\"idEvento\":\"$(idEvento)\",\"detalles\":\"$(extraData)\",\"tag\":\"app_unica\"}}}";
 
 						public const string iOS = "{ \"aps\" : {\"alert\" : \"$(alertMessage)\"}, \"action\" : \"$(alertAction)\" }";
 				}
 
 				public class Silent
 				{
-						//public const string Android = "{\"message\":{\"notification\":{\"title\":\"$(alertTitle)\",\"body\":\"$(alertMessage)\"},\"android\":{\"priority\":\"high\"},\"data\":{\"action\":\"$(alertAction)\",\"idEvento\":\"$(idEvento)\",\"detalles\":\"$(extraData)\"}}}";
-
-						public const string Android = "{\"message\":{\"android\":{\"priority\":\"high\",\"notification\":{\"title\":\"$(alertTitle)\",\"body\":\"$(alertMessage)\",\"tag\":\"app_unica\"}},\"data\":{\"action\":\"$(alertAction)\",\"idEvento\":\"$(idEvento)\",\"detalles\":\"$(extraData)\"}}}";
+						public const string Android = "{\"message\":{\"android\":{\"priority\":\"high\"},\"data\":{\"title\":\"$(alertTitle)\",\"body\":\"$(alertMessage)\",\"action\":\"$(alertAction)\",\"idEvento\":\"$(idEvento)\",\"detalles\":\"$(extraData)\",\"tag\":\"app_unica\"}}}";
 
 						public const string iOS = "{ \"aps\" : {\"content-available\" : 1, \"apns-priority\": 5, \"sound\" : \"\", \"badge\" : 0}, \"message\" : \"$(alertMessage)\", \"action\" : \"$(alertAction)\" }";
 				}
